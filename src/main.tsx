573dcdd48cf124748aa9c38964bbb8ab65422086
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2023-03-02 16:23:07 +0000

    Add CD

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client.js';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.querySelector('#root')!).render(
-	<React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
 		<App />
-	</React.StrictMode>,
+	</StrictMode>,
 );
